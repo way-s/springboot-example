d547de5940bca9789fbee838f9b2c4c03a529324
way-s <2509980279@qq.com>
2022-05-19 10:37:49 +0800

    update spring security

--- springboot-security/src/main/java/com/example/security/controller/WebSecurityController.java
+++ springboot-security/src/main/java/com/example/security/controller/WebSecurityController.java
@@ -1,10 +1,13 @@
 package com.example.security.controller;
 
 import com.example.entity.vo.Result;
+import com.example.security.entity.UserDetail;
 import com.example.security.service.IAuthService;
 import com.example.security.service.IUserInfoStService;
 import com.example.security.vo.LoginVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -79,6 +82,24 @@ public class WebSecurityController {
         log.info("user 接口被访问");
         return Result.success("user");
     }
+
+    /**
+     * 获取系统用户
+     * http://localhost:8888/st/pal
+     *
+     * @return
+     */
+    @GetMapping("/pal")
+    public Object testGet() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            return Result.fail();
+        }
+        log.info("authentication: {}", authentication);
+        UserDetail principal = (UserDetail) authentication.getPrincipal();
+        return Result.success(principal);
+    }
+
 }
 
 
